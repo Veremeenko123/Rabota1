42e7634ac58fd05679269c8761e8ce7651dc042b
danil <danilveremeenko323@gmail.com>
2023-11-13 12:17:16 +0300

    Изменения

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        int number = 0;
+        String numberSum;
+        int sum = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("F://МДК//text.txt"))) {
+            String line;
+            String boldText = "\u001B[1mОценка меньше 3-х баллов: \u001B[0m";
+            System.out.println(boldText + "\n");
+            while ((line = bufferedReader.readLine()) != null) {
+                number++;
+                numberSum = line.replaceAll("\\D", "");
+                sum = sum + Integer.parseInt(numberSum);
+            }
+        } catch (IOException e) {
+            System.out.println("Файл не найден");
         }
     }
 }
